c7aa46057a897afed827f7fcfa134c59107de7dd
Christian Knapp <christian.alex.knapp@icloud.com>
2024-08-24 12:55:32 +0200

    fixed physical actions being not recognized

--- main/src/main/java/de/wintervillage/main/plot/listener/block/PlayerInteractListener.java
+++ main/src/main/java/de/wintervillage/main/plot/listener/block/PlayerInteractListener.java
@@ -2,14 +2,13 @@ package de.wintervillage.main.plot.listener.block;
 
 import de.wintervillage.main.WinterVillage;
 import de.wintervillage.main.plot.Plot;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -29,8 +28,6 @@ public class PlayerInteractListener implements Listener {
 
         Block clickedBlock = event.getClickedBlock();
         if (clickedBlock == null) return;
-        // ignore non-interactable blocks
-        if (!(clickedBlock.getType().isInteractable() && clickedBlock.getType().isBlock())) return;
 
         Plot plot = this.winterVillage.plotHandler.byBounds(clickedBlock.getLocation());
         if (plot == null) return;
@@ -40,8 +37,10 @@ public class PlayerInteractListener implements Listener {
         if (plot.owner().equals(player.getUniqueId()) || plot.members().contains(player.getUniqueId())) return;
 
         // cancel interactions with blocks in plots that the player is not a member of
-        // event.setUseItemInHand(Event.Result.DENY);
-        event.setUseInteractedBlock(Event.Result.DENY);
-        this.winterVillage.plotHandler.deny(player, clickedBlock.getLocation());
+        if (event.getAction() == Action.PHYSICAL || (clickedBlock.getType().isInteractable() && clickedBlock.getType().isBlock())) {
+            event.setUseInteractedBlock(Event.Result.DENY);
+            this.winterVillage.plotHandler.deny(player, clickedBlock.getLocation());
+            return;
+        }
     }
 }
