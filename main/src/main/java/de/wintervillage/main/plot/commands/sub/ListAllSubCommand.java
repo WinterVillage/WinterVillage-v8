640eecfc57f8d1763d4ddc003901c0f1ff6b74ba
Christian Knapp <christian.alex.knapp@icloud.com>
2024-08-24 23:08:21 +0200

    added `listAll` sub command message

--- main/src/main/java/de/wintervillage/main/plot/commands/sub/ListAllSubCommand.java
+++ main/src/main/java/de/wintervillage/main/plot/commands/sub/ListAllSubCommand.java
@@ -5,6 +5,11 @@ import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import de.wintervillage.main.WinterVillage;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.luckperms.api.model.group.Group;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -30,8 +35,38 @@ public class ListAllSubCommand {
                         return 1;
                     }
 
-                    // TODO: message
+                    player.sendMessage(Component.translatable("wintervillage.commands.plot.list-plots"));
+
+                    this.winterVillage.plotHandler.getPlotCache().forEach(plot -> {
+                        this.winterVillage.plotHandler.lookupUsers(plot).thenAccept(plotUsers -> {
+                            Group highestGroup = this.winterVillage.playerHandler.highestGroup(plotUsers.owner());
+
+                            player.sendMessage(this.buildFromContext(
+                                    plot.name(),
+                                    highestGroup.getCachedData().getMetaData().getMetaValue("color") + plotUsers.owner().getUsername(),
+                                    plot.uniqueId().toString())
+                            );
+                        });
+                    });
                     return Command.SINGLE_SUCCESS;
                 });
+    }
+
+    private Component buildFromContext(String arg0, String arg1, String arg2) {
+        return Component.text()
+                .append(Component.text("-", NamedTextColor.DARK_GRAY)
+                        .decorate(TextDecoration.BOLD))
+                .append(Component.space())
+                .append(Component.text(arg0, NamedTextColor.YELLOW)
+                        .clickEvent(ClickEvent.runCommand("/gs info " + arg2)))
+                .append(Component.text(" von ", NamedTextColor.WHITE))
+                .append(MiniMessage.miniMessage().deserialize(arg1))
+                .append(Component.space())
+                .append(Component.text("[Delete]", NamedTextColor.RED)
+                        .clickEvent(ClickEvent.runCommand("/gs delete " + arg2)))
+                .append(Component.space())
+                .append(Component.text("[Teleport]", NamedTextColor.BLUE)
+                        .clickEvent(ClickEvent.runCommand("/gs tp " + arg2)))
+                .build();
     }
 }
