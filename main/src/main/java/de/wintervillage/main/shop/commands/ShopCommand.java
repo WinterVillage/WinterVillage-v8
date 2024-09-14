f36eeb2e084e4832f53cd7631a19e5d62aea75b6
Christian Knapp <christian.alex.knapp@icloud.com>
2024-09-14 09:48:56 +0200

    added command argument, translation messages
    - added changeOwner sub command
    - eng & ger translation messages
    - CombinedUserShop

--- main/src/main/java/de/wintervillage/main/shop/commands/ShopCommand.java
+++ main/src/main/java/de/wintervillage/main/shop/commands/ShopCommand.java
@@ -2,6 +2,7 @@ package de.wintervillage.main.shop.commands;
 
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import de.wintervillage.main.WinterVillage;
+import de.wintervillage.main.shop.commands.sub.ChangeOwnerSubCommand;
 import de.wintervillage.main.shop.commands.sub.ChangePriceSubCommand;
 import de.wintervillage.main.shop.commands.sub.DeleteSubCommand;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
@@ -14,9 +15,11 @@ import java.util.List;
 public class ShopCommand {
 
     /**
+     * /shop listAll            TODO:
+     * /shop info (uniqueId)    TODO:
      * /shop delete (uniqueId) | Deletes a shop
+     * /shop changeOwner <newOwner> | Changes the owner of a shop
      * /shop changePrice <newPrice> (uniqueId) | Changes the price of a shop
-     *
      */
 
     public ShopCommand(Commands commands) {
@@ -25,7 +28,8 @@ public class ShopCommand {
         final LiteralArgumentBuilder<CommandSourceStack> builder = Commands.literal("shop")
                 .requires((source) -> source.getSender() instanceof Player)
                 .then(new DeleteSubCommand().build())
-                .then(new ChangePriceSubCommand().build());
+                .then(new ChangePriceSubCommand().build())
+                .then(new ChangeOwnerSubCommand().build());
         commands.register(winterVillage.getPluginMeta(), builder.build(), "Manage shops", List.of());
     }
 }
