5834560856839228928333e6fb247b74818e2835
Christian Knapp <christian.alex.knapp@icloud.com>
2024-09-15 16:15:26 +0200

    wrong start date corrected

--- main/src/main/java/de/wintervillage/main/calendar/CalendarHandler.java
+++ main/src/main/java/de/wintervillage/main/calendar/CalendarHandler.java
@@ -23,7 +23,7 @@ public class CalendarHandler {
         this.winterVillage = JavaPlugin.getPlugin(WinterVillage.class);
         this.days = new ArrayList<>();
 
-        this.startDate = LocalDate.of(2024, Month.DECEMBER, 12);
+        this.startDate = LocalDate.of(2024, Month.DECEMBER, 1);
         this.endDate = LocalDate.of(2025, Month.JANUARY, 6); // TODO: set to project end date
 
         this.forceUpdate();
