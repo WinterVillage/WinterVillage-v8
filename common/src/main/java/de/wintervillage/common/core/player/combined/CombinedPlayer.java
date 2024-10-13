8f4ea4c645d3aa854678af14d9430cf260261197
Christian Knapp <christian.alex.knapp@icloud.com>
2024-10-13 14:06:06 +0200

    removed unnecessary because of Pair

--- common/src/main/java/de/wintervillage/common/core/player/combined/CombinedPlayer.java
+++
@@ -1,8 +0,0 @@
-package de.wintervillage.common.core.player.combined;
-
-import de.wintervillage.common.core.player.WinterVillagePlayer;
-import net.luckperms.api.model.user.User;
-
-public record CombinedPlayer(User user, WinterVillagePlayer winterVillagePlayer) {
-
-}
