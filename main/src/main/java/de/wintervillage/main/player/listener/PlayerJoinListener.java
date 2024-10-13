e0fee58701430b2ae494ecfef763b6bdf7580049
Christian Knapp <christian.alex.knapp@icloud.com>
2024-10-13 23:08:11 +0200

    added join/ quit messages

--- main/src/main/java/de/wintervillage/main/player/listener/PlayerJoinListener.java
+++ main/src/main/java/de/wintervillage/main/player/listener/PlayerJoinListener.java
@@ -2,8 +2,8 @@ package de.wintervillage.main.player.listener;
 
 import de.wintervillage.main.WinterVillage;
 import net.kyori.adventure.text.Component;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.luckperms.api.model.group.Group;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -26,6 +26,11 @@ public class PlayerJoinListener implements Listener {
     @EventHandler(priority = EventPriority.LOWEST)
     public void execute(PlayerJoinEvent event) {
         final Player player = event.getPlayer();
+
+        Group highestGroup = this.winterVillage.playerHandler.highestGroup(player);
+        event.joinMessage(Component.translatable("wintervillage.player-join",
+                MiniMessage.miniMessage().deserialize(highestGroup.getCachedData().getMetaData().getMetaValue("color") + player.getName())
+        ));
 
         // player data being loaded
         this.winterVillage.playerHandler.apply(player, player.getUniqueId());
--- main/src/main/java/de/wintervillage/main/player/listener/PlayerQuitListener.java
+++ main/src/main/java/de/wintervillage/main/player/listener/PlayerQuitListener.java
@@ -1,6 +1,9 @@
 package de.wintervillage.main.player.listener;
 
 import de.wintervillage.main.WinterVillage;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.luckperms.api.model.group.Group;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -20,6 +23,11 @@ public class PlayerQuitListener implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST) // HIGHEST, so every listener can modify the player before he's being saved
     public void execute(PlayerQuitEvent event) {
         final Player player = event.getPlayer();
+
+        Group highestGroup = this.winterVillage.playerHandler.highestGroup(player);
+        event.quitMessage(Component.translatable("wintervillage.player-quit",
+                MiniMessage.miniMessage().deserialize(highestGroup.getCachedData().getMetaData().getMetaValue("color") + player.getName())
+        ));
 
         // delete scoreboard data
         this.winterVillage.scoreboardHandler.removeScoreboard(player.getUniqueId());
