7fe0e160665efda1f6ec162a7935d55daa3fd8dd
Christian Knapp <christian.alex.knapp@icloud.com>
2024-10-16 20:45:52 +0200

    customName -> PDC

--- main/src/main/java/de/wintervillage/main/specialitems/items/SpecialItem_SantasBow.java
+++ main/src/main/java/de/wintervillage/main/specialitems/items/SpecialItem_SantasBow.java
@@ -5,6 +5,7 @@ import de.wintervillage.main.specialitems.SpecialItems;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Monster;
 import org.bukkit.entity.Player;
@@ -13,55 +14,62 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 
 import java.util.Random;
 
 public class SpecialItem_SantasBow extends SpecialItem {
 
-    public SpecialItem_SantasBow(){
+    private final NamespacedKey key;
+
+    public SpecialItem_SantasBow() {
         super();
         ItemStack item = SpecialItems.getSpecialItem(Component.text("Santa's Bow"), Material.BOW, 1, true);
         this.setItem(item);
         this.setNameStr("santas_bow");
+
+        this.key = new NamespacedKey("wintervillage:specialitems", "santas_bow");
     }
 
     @EventHandler
-    public void onEntityShootBow(EntityShootBowEvent event){
-        if(event.getEntity() instanceof Player player){
-            ItemStack bow = player.getInventory().getItemInMainHand();
-            if(isSpecialitem(bow)){
-                event.getProjectile().customName(Component.text("santa_arrow"));
-            }
+    public void onEntityShootBow(EntityShootBowEvent event) {
+        if (event.getEntity() instanceof Player && event.getBow() != null) {
+            if (!isSpecialitem(event.getBow())) return;
+
+            PersistentDataContainer container = event.getProjectile().getPersistentDataContainer();
+            container.set(this.key, PersistentDataType.BOOLEAN, true);
         }
     }
 
     @EventHandler
-    public void onEntityDamageEvent(EntityDamageByEntityEvent event){
-        if(event.getEntity() instanceof Arrow arrow){
-            if(arrow.customName() != null && arrow.customName().contains(Component.text("santa_arrow"))){
-                if(event.getEntity() instanceof Monster monster){
-                    Random rdm = new Random();
-                    if(rdm.nextInt(2) == 0){
-                        event.setDamage(monster.getHealth() * 2);
-                    }
-                }
-            }
+    public void onEntityDamageEvent(EntityDamageByEntityEvent event) {
+        if (event.getDamager() instanceof Arrow arrow) {
+            PersistentDataContainer container = arrow.getPersistentDataContainer();
+
+            if (!container.has(this.key, PersistentDataType.BOOLEAN)) return;
+
+            if (!(event.getEntity() instanceof Monster monster)) return;
+
+            Random rnd = new Random();
+            if (rnd.nextInt(2) == 0) event.setDamage(monster.getHealth() * 2);
         }
     }
 
     @EventHandler
-    public void onEntityDeathEvent(EntityDeathEvent event){
-        if(event.getEntity().getLastDamageCause() instanceof EntityDamageByEntityEvent damageEvent){
-            if(damageEvent.getDamager() instanceof Arrow arrow){
-                if(arrow.customName() != null && arrow.customName().contains(Component.text("santa_arrow"))){
-                    if(!event.getDrops().isEmpty()){
-                        Location location = event.getEntity().getLocation();
+    public void onEntityDeathEvent(EntityDeathEvent event) {
+        if (!(event.getEntity().getLastDamageCause() instanceof EntityDamageByEntityEvent damageEvent)) return;
 
-                        event.getDrops().forEach(drop -> location.getWorld().dropItemNaturally(location, drop));
-                        event.setDroppedExp(event.getDroppedExp() * 2);
-                    }
-                }
-            }
+        if (!(damageEvent.getDamager() instanceof Arrow arrow)) return;
+        PersistentDataContainer container = arrow.getPersistentDataContainer();
+
+        if (!container.has(this.key, PersistentDataType.BOOLEAN)) return;
+
+        if (!event.getDrops().isEmpty()) {
+            Location location = event.getEntity().getLocation();
+
+            event.getDrops().forEach(drop -> location.getWorld().dropItemNaturally(location, drop));
+            event.setDroppedExp(event.getDroppedExp() * 2);
         }
     }
 
